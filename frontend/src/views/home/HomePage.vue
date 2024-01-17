5cb6f7e2aede5839b7ec94ebbf4e4a19b421fe77
vuthanhnhan <vuthanhnhan1411@gmail.com>
2024-01-17 23:31:56 +0700

    feature: change env

--- frontend/src/views/home/HomePage.vue
+++ frontend/src/views/home/HomePage.vue
@@ -15,13 +15,6 @@
           <div class="title">{{ categoryItem.name }}</div>
         </div>
       </div>
-
-      <!-- Button go to add product -->
-      <div class="add-product-button">
-        <el-button type="primary" @click="onHandleClickAddProduct">
-          Add new product
-        </el-button>
-      </div>
     </div>
 
   </div>
